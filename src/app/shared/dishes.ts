f435f596f5ed6df9c1a42d79f0fa0032a5d9e8c6
Kesavan Anandham <kesavan.anandham@gmail.com>
2017-07-25 21:22:55 +0530

    Single Page Applications Part 1

--- src/app/shared/dishes.ts
+++ src/app/shared/dishes.ts
@@ -3,9 +3,11 @@ import { Dish } from './dish';
 
 export const DISHES: Dish[] = [
     {
+        id: 0,
         name: 'Uthappizza',
         image: '/assets/images/uthappizza.png',
         category: 'mains',
+        featured: true,
         label: 'Hot',
         price: '4.99',
         description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
@@ -43,9 +45,11 @@ export const DISHES: Dish[] = [
         ]
     },
     {
+        id: 1,
         name: 'Zucchipakoda',
         image: '/assets/images/zucchipakoda.png',
         category: 'appetizer',
+        featured: false,
         label: '',
         price: '1.99',
         description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
@@ -83,9 +87,11 @@ export const DISHES: Dish[] = [
         ]
     },
     {
+        id: 2,
         name: 'Vadonut',
         image: '/assets/images/vadonut.png',
         category: 'appetizer',
+        featured: false,
         label: 'New',
         price: '1.99',
         description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
@@ -123,9 +129,11 @@ export const DISHES: Dish[] = [
         ]
     },
     {
+        id: 3,
         name: 'ElaiCheese Cake',
         image: '/assets/images/elaicheesecake.png',
         category: 'dessert',
+        featured: false,
         label: '',
         price: '2.99',
         description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
