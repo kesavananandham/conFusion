643bee1aad65b9395583aed94ecc15599a43e6e2
Kesavan Anandham <kesavan.anandham@gmail.com>
2017-07-30 16:51:34 +0530

    Reactive Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '@angular/material';
@@ -41,6 +41,7 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     FlexLayoutModule,
     MaterialModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   entryComponents: [ LoginComponent ],
